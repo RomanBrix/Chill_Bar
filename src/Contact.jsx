4760af6761ab1b422203df589be47519d71c0465
Roman <Stetcukroman@gmail.com>
2023-01-27 09:28:43 +0200

    prod

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,16 +1,15 @@
 export default {
-        telephone1: '+380683737157',
-        prettyTel1: '+380 68 37 37 157',
-        telephone2: '+380683737157',
-        prettyTel2: '+380 68 37 37 157',
-        maiL: 'vashemilo@google.com',
-        tiktok: 'https://www.tiktok.com/ru-RU',
-        tiktok_tag: '@tiktok',
-        insta: 'https://www.instagram.com/',
-        insta_tag: '@insta',
-        telega: 'tg://resolve?domain=abcd',
-        telega_tag:'@telega',
-        viber: 'viber://contact?number=%2B380635487588',
-        viber_tag: '@viber'
-    }
-
+    telephone1: "+380683737157",
+    prettyTel1: "+380 68 37 37 157",
+    telephone2: "+380683737157",
+    prettyTel2: "+380 68 37 37 157",
+    maiL: "information@chillbar.com.ua",
+    tiktok: "https://www.tiktok.com/ru-RU",
+    tiktok_tag: "@tiktok",
+    insta: "https://www.instagram.com/",
+    insta_tag: "@insta",
+    telega: "tg://resolve?domain=abcd",
+    telega_tag: "@telega",
+    viber: "viber://contact?number=%2B380635487588",
+    viber_tag: "@viber",
+};
